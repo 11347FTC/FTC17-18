8aa4af789dbb3a0b31fb3b401f22d541a68c7876
Ganondorf <FTC11347@gmail.com>
2016-11-21 20:14:53 -0500

    update1 from home

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZippyAutoRedAlliance1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZippyAutoRedAlliance1.java
@@ -5,12 +5,16 @@ import android.media.MediaCodecInfo;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+import java.sql.Time;
 
 /**
  * Created by Ganondorf on 11/16/2016.
@@ -22,80 +26,147 @@ public class ZippyAutoRedAlliance1 extends LinearOpMode {
     //For Mechanum
     DcMotor backLeft = null;
     DcMotor backRight = null;
-    Servo leftPoker = null;
-    Servo rightPoker = null;
+    Servo leftPoker;
+    Servo rightPoker;
     //Low Goal Shooter Motor
     DcMotor lowGoal = null;
+    ColorSensor beacon = null;
     private ElapsedTime runtime = new ElapsedTime();
 
 
+    double DriveSpeed = 0.5;
+    double TurnSpeed = 0.6;
+    double ShootPower = 0.5;
+    //complete
+
     @Override
     public void runOpMode() {
-        // Send telemetry message to signify robot waiting;
-        telemetry.addData("Status", "Resetting Encoders");    //
-        telemetry.update();
-        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        // Send telemetry message to indicate successful Encoder reset
-        telemetry.addData("Path0", "Starting at %7d :%7d",
-                backLeft.getCurrentPosition(),
-                backRight.getCurrentPosition());
-        telemetry.update();
+        //frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        //frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        //For Mechanum
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        backRight = hardwareMap.dcMotor.get("backRight");
 
-        idle();
-        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftPoker = hardwareMap.servo.get("leftPoker");
+        rightPoker = hardwareMap.servo.get("rightPoker");
+
+        //frontLeftMotor.setChannelMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+        //frontRightMotor.setChannelMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+        //For Mechanum
+
+        //Right Motor needs to be reversed
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+        //frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftPoker.setPosition(1);
+        rightPoker.setPosition(0);
+        //Low Goal Shooter
+        lowGoal = hardwareMap.dcMotor.get("lowGoal");
+        //Color Sensor
+        beacon = hardwareMap.colorSensor.get("beacon");
+
         waitForStart();
-        encoderDrive(0.6, 48, 48, 5.0);
+        beacon.enableLed(false);
+        leftPoker.setPosition(1);
+        rightPoker.setPosition(0);
+
+        //Move Forward for 2 seconds
+        DriveForwardTime(DriveSpeed,2000);
+
+        //Turn for 1 second
+        TurnLeftTime(TurnSpeed, 1000);
+        //Move Forward for 1 second
+        DriveForwardTime(DriveSpeed, 1000);
+        //Sense beacons
+        SenseRed();
+        DriveBackwardTime(DriveSpeed,1500);
+        TurnLeftTime(TurnSpeed,500);
+        DriveForwardTime(DriveSpeed,2000);
+        Shoot(ShootPower,3000);
     }
-
-    public void encoderDrive(double speed,
-                             double leftInches, double rightInches,
-                             double timeoutS) {
-        int newLeftTarget;
-        int newRightTarget;
-
-        // Ensure that the opmode is still active
-        if (opModeIsActive()) {
-
-            // Determine new target position, and pass to motor controller
-            newLeftTarget = backLeft.getCurrentPosition() + (int) (leftInches * 229.19);
-            newRightTarget = backRight.getCurrentPosition() + (int) (rightInches * 229.19);
-            backLeft.setTargetPosition(newLeftTarget);
-            backRight.setTargetPosition(newRightTarget);
-
-            // Turn On RUN_TO_POSITION
-            backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            // reset the timeout time and start motion.
-            runtime.reset();
-            backLeft.setPower(Math.abs(speed));
-            backRight.setPower(Math.abs(speed));
-
-            // keep looping while we are still active, and there is time left, and both motors are running.
-            while (opModeIsActive() &&
-                    (runtime.seconds() < timeoutS) &&
-                    (backLeft.isBusy() && backRight.isBusy())) {
-
-                // Display it for the driver.
-                telemetry.addData("Path1", "Running to %7d :%7d", newLeftTarget, newRightTarget);
-                telemetry.addData("Path2", "Running at %7d :%7d",
-                        backLeft.getCurrentPosition(),
-                        backRight.getCurrentPosition());
-                telemetry.update();
-            }
-
-            // Stop all motion;
-            backLeft.setPower(0);
-            backRight.setPower(0);
-
-            // Turn off RUN_TO_POSITION
-            backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-            //  sleep(250);   // optional pause after each move
+    //A lot of Methods for Auto
+    public void DriveForward(double power){
+        backLeft.setPower(power);
+        backRight.setPower(-power);
+    }
+    public void DriveBackward(double power){
+        backLeft.setPower(-power);
+        backRight.setPower(power);
+    }
+    public void TurnLeft(double power){
+        backLeft.setPower(power);
+        backRight.setPower(power);
+    }
+    public void TurnRight(double power){
+        backLeft.setPower(-power);
+        backRight.setPower(-power);
+    }
+    public void TimeElapsed(long time){
+        while (opModeIsActive() && (runtime.seconds() < time)) {
+            telemetry.addData("Path", "Leg : %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
         }
     }
-}
+    public void DriveForwardTime(double power, long time){
+        DriveForward(power);
+        TimeElapsed(time);
+    }
+    public void DriveBackwardTime(double power, long time){
+        DriveBackward(power);
+        TimeElapsed(time);
+    }
+    public void TurnLeftTime (double power, long time){
+        TurnLeft(power);
+        TimeElapsed(time);
+    }
+    public void TurnRightTime (double power, long time){
+        TurnRight(power);
+        TimeElapsed(time);
+    }
+    public void PokeRight (double position, double power, long time){
+        rightPoker.setPosition(position);
+        DriveForwardTime(power,time);
+    }
+    public void PokeLeft (double position, double power, long time){
+        leftPoker.setPosition(position);
+        DriveForwardTime(power,time);
+    }
+    public void SenseRed (){
+        telemetry.addData("3 Red ", beacon.red());
+        telemetry.addData("4 Green ", beacon.green());
+        telemetry.addData("5 Blue ", beacon.blue());
 
+        if (beacon.red() > beacon.blue() || beacon.red() > beacon.green()) {
+            PokeLeft(0.5, DriveSpeed, 500);
+        } else if (beacon.blue() > beacon.red() || beacon.blue() > beacon.green()) {
+            PokeRight(0.5, DriveSpeed, 500);
+        }
+        else {
+            //Just incase it doesn't sense anything
+
+            DriveForwardTime(DriveSpeed, 500);
+
+        }
+    }
+    public void SenseBlue (){
+        telemetry.addData("3 Red ", beacon.red());
+        telemetry.addData("4 Green ", beacon.green());
+        telemetry.addData("5 Blue ", beacon.blue());
+
+        if (beacon.blue() > beacon.red() || beacon.blue() > beacon.green()) {
+            PokeLeft(0.5, DriveSpeed, 500);
+        } else if (beacon.red() > beacon.blue() || beacon.red() > beacon.green()) {
+            PokeRight(0.5, DriveSpeed, 500);
+        }
+        else {
+            //Just incase it doesn't sense anything
+
+            DriveForwardTime(DriveSpeed, 500);
+
+        }
+    }
+    public void Shoot (double power, long time){
+        lowGoal.setPower(power);
+        TimeElapsed(time);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZippyTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZippyTeleOp.java
@@ -44,12 +44,10 @@ public class ZippyTeleOp extends OpMode {
         backLeft.setDirection(DcMotor.Direction.FORWARD);
         backRight.setDirection(DcMotor.Direction.REVERSE);
         //frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
-        leftPoker.setPosition(0);
-        rightPoker.setPosition(1);
+        leftPoker.setPosition(1);
+        rightPoker.setPosition(0);
         //Low Goal Shooter
         lowGoal = hardwareMap.dcMotor.get("lowGoal");
-        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
     @Override
     public void loop() {
@@ -63,12 +61,12 @@ public class ZippyTeleOp extends OpMode {
 
         if (gamepad2.left_bumper){
             //poking for beacons
-            leftPoker.setPosition(0.55);
-            rightPoker.setPosition(1);}
+            leftPoker.setPosition(0.5);
+            rightPoker.setPosition(0);}
 
         if (gamepad2.right_bumper){
-            rightPoker.setPosition(0.45);
-            leftPoker.setPosition(0);}
+            rightPoker.setPosition(0.5);
+            leftPoker.setPosition(1);}
 
 
 //Looked at the sample code
@@ -85,21 +83,22 @@ public class ZippyTeleOp extends OpMode {
         //Tank Drive with only two motors
 
 
-        if (gamepad2.a) {
+        /*if (gamepad2.a) {
             backLeft.setPower(powery/2);
             backRight.setPower(powerv/2.1);
                     }
         if (gamepad2.b) {
             backLeft.setPower(powery/4);
             backRight.setPower(powerv/4.1);
-        }
+        }*/
+        //failed turbo mode
         /*driving for the mechanums
         *frontLeftMotor.setPower(powery - powerx + powerz);
         *frontRightMotor.setPower(powery + powerx - powerz);
         *backRightMotor.setPower(powery - powerx - powerz);
         *backLeft.setPower(powery + powerx + powerz);
         */
-
+//Turbo Mode doesn't seem to work
         //LowGoal
         lowGoal.setPower(goalPower);
         //need to test these asap
